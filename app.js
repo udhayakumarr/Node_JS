0bbb7a260b6d245c591ba24ee7f6675410d6ae3f
udhaya kumar <rudhaya7896@gmail.com>
2024-04-29 20:25:31 +0530

    cleaning up the code using EventEmitter

--- app.js
+++ app.js
@@ -3,24 +3,26 @@ const fs = require("fs/promises");
 (async () => {
   const commandFileHandler = await fs.open("./command.txt", "r");
 
-  const watcher = fs.watch("./command.txt");
+  commandFileHandler.on("change", async () => {
+    const size = (await commandFileHandler.stat()).size;
+    const buffer = Buffer.alloc(size);
+    const offset = 0;
+    const length = buffer.byteLength;
+    const position = 0;
 
+    const content = await commandFileHandler.read(
+      buffer,
+      offset,
+      length,
+      position
+    );
+
+    console.log(content.buffer);
+  });
+  const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
     if (event.eventType === "change") {
-      const size = (await commandFileHandler.stat()).size;
-      const buffer = Buffer.alloc(size);
-      const offset = 0;
-      const length = buffer.byteLength;
-      const position = 0;
-
-      const content = await commandFileHandler.read(
-        buffer,
-        offset,
-        length,
-        position
-      );
-
-      console.log(content)
+      commandFileHandler.emit("change");
     }
   }
 })();
