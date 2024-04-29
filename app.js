fd449c3925158fe0f4ca2cf1f9994fe74eaab3b4
udhaya kumar <rudhaya7896@gmail.com>
2024-04-29 21:34:25 +0530

    read the data from buffer in utf 8 formot

--- app.js
+++ app.js
@@ -4,20 +4,29 @@ const fs = require("fs/promises");
   const commandFileHandler = await fs.open("./command.txt", "r");
 
   commandFileHandler.on("change", async () => {
+    // get the size of our file
     const size = (await commandFileHandler.stat()).size;
+
+    // allocate size to the buffer
     const buffer = Buffer.alloc(size);
+
+    // the location at wihich we want to fiiling our buffer
     const offset = 0;
+
+    // how many bytes we want to read
     const length = buffer.byteLength;
+
+    // the postion that we want to start reading the file from
     const position = 0;
 
-    const content = await commandFileHandler.read(
-      buffer,
-      offset,
-      length,
-      position
-    );
+    // we always the read the content from start to end
+     await commandFileHandler.read(buffer, offset, length, position);
 
-    console.log(content.buffer);
+    // console.log(buffer)
+
+    const content = buffer.toString("utf-8");
+
+    console.log("content", content);
   });
   const watcher = fs.watch("./command.txt");
   for await (const event of watcher) {
